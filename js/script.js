545d3ed3dedd5dd94ba20e9c077acb57079b9052
karan <codeandcore@gmail.com>
2024-11-27 17:26:25 +0530

    about page

--- js/script.js
+++ js/script.js
@@ -122,7 +122,27 @@ jQuery(document).ready(function($){
         $(this).next().slideToggle();
     });
 
+    $('[data-pop="megnify"]').each(function(){
+        var _this = $(this);
+        _this.find('.image-popup-vertical-fit').magnificPopup({
+            type: 'image',
+              mainClass: 'mfp-with-zoom', 
+              gallery:{
+                        enabled:true
+                    },
+            
+              zoom: {
+                enabled: true, 
+                duration: 300, // duration of the effect, in milliseconds
+                easing: 'ease-in-out', // CSS transition easing function
+                opener: function(openerElement) {
+                  return openerElement.is('img') ? openerElement : openerElement.find('img');
+              }
+            }        
+        });
 
+    })
+        
 
 
 });
